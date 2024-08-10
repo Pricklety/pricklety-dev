f15ba780f09409d4234546ff1e92fba7840b5cad
Pricklety <ramulickarlo15@gmail.com>
2024-08-10 16:05:54 +0200

    fixed context part 2

--- app/context/context.js
+++ app/context/context.js
@@ -3,14 +3,14 @@
 
 import { createContext, useState } from "react";
 
-export const context = createContext();
+export const Context = createContext();
 
 export function Provider({ children }) {
   const [value, setValue] = useState("Hello, world!");
 
   return (
-    <context.Provider value={{ value, setValue }}>
+    <Context.Provider value={{ value, setValue }}>
       {children}
-    </context.Provider>
+    </Context.Provider>
   );
 }
