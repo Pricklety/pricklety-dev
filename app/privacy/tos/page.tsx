87a41306bfd1a39a44cce2650d079421b249bd0f
Pricklety <ramulickarlo15@gmail.com>
2024-10-19 18:34:34 +0200

    added privacy policy and tos

---
+++ app/privacy/tos/page.tsx
@@ -0,0 +1,13 @@
+// app/devs/page.tsx
+export default function Devs() {
+    return (
+      <main>
+        <div className="flex flex-col justify-center items-center h-screen">
+          <h1>Dev and staff list</h1>
+          <br />
+          <p>This is the about page for developers.</p>
+        </div>
+      </main>
+    );
+  }
+  
