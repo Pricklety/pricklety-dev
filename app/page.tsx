be3656991acd433784f4008317a4bc971f557bfd
Pricklety <ramulickarlo15@gmail.com>
2024-12-09 17:55:25 +0100

    added info on main page header

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ const App: React.FC = () => {
                     <li><a href="https://jammerdash.fandom.com">wiki</a></li>
                     <li><a href="#community" className="disabled">community</a></li>
                     <li><a href="./store" className="disabled">store</a></li> 
+                    <li><a href="./info/game">info</a></li> 
                 </ul>
                 </nav>
             </header>
